054f528fdab4b34abb44b15df73bbe82e4ed3df2
alexandrei0805 <116917004+alexandrei0805@users.noreply.github.com>
2023-01-06 14:14:20 +0200

    Update Mario-first.c

--- ProblemSet1/Mario-first.c
+++ ProblemSet1/Mario-first.c
@@ -1,1 +1,16 @@
+#include <csbootcamp.h>
+#include <stdio.h>
 
+int main(void)
+{
+for(int i=0;i<8;i++)
+    {
+        printf("\n");
+        for(int i = 0;i<8;i++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    
+}
