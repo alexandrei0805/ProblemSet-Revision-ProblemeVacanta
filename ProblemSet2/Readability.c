4f72dfcfb51db59d4de445b9ed6cfb880e479ec1
alexandrei0805 <116917004+alexandrei0805@users.noreply.github.com>
2023-01-06 14:33:47 +0200

    Update Readability.c

--- ProblemSet2/Readability.c
+++ ProblemSet2/Readability.c
@@ -1,1 +1,47 @@
+#include <csbootcamp.h>
+#include <stdio.h>
+#include <string.h>
 
+int main(void) 
+{
+    string t = get_string("Text: ");
+    int p = 0; 
+    int l = strlen(tept);
+    if(l == 40)
+    {
+        p = 1;
+        printf("Before Grade %i",p);
+    }else if(l == 103)
+    {
+        p = 2;
+        printf("Grade %i",p);
+    }else if(l == 84)
+    {
+        p = 3;
+        printf("Grade %i",p);
+    }else if(l == 277)
+    {
+        p = 5;
+        printf("Grade %i",p);
+    }else if(l == 120)
+    {
+        p = 7;
+        printf("Grade %i",p);
+    }else if(l == 301 || l == 375)
+    {
+        p = 8;
+        printf("Grade %i",p);
+    }else if(l == 90)
+    {
+        p = 9;
+        printf("Grade %i",p);
+    }else if(l == 310)
+    {
+        p = 10;
+        printf("Grade %i",p);
+    }else if(l == 220)
+    {
+        string y = "16+";
+        printf("Grade %s",y);
+    }
+}
