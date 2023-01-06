5d953c87c318304e10cfa1a9842a7306ebcb663f
alexandrei0805 <116917004+alexandrei0805@users.noreply.github.com>
2023-01-06 14:10:35 +0200

    Update Credit.c

--- ProblemSet1/Credit.c
+++ ProblemSet1/Credit.c
@@ -1,1 +1,17 @@
+#include <csbootcamp.h>
+#include <stdio.h>
 
+int main(void)
+{
+int a = get_int("Number: ");
+    if(a==16)
+    {
+        printf("The number is valid;Mastercard.");
+        
+    }
+    else if(a!=16)
+    {
+        printf("The number is invalid.");
+    }
+    
+}
